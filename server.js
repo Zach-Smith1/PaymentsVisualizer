967544514e74ac61cb54ca27f446751693e64012
Zach Smith <zachsmithglobal@icloud.com>
2023-11-26 10:05:19 -0800

    login testing

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-// SERVER CURRENTLY NOT USED
-// APP UNDER DEVELOPMENT
-
-
+const { arMiddleware, cookieParser, fullLogout, requireLogin } = require('@authrocket/authrocket-middleware');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cookieParser())
+app.use('/logout', fullLogout)
+app.use(arMiddleware({
+  authrocket: {
+    loginrocketUrl: 'https://valid-prize-5f87.e2.loginrocket.com/'
+  }
+}))
+app.use(requireLogin)
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
